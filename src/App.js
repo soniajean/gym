87e1792ead22913af4b7539ddafbf10af6b14823
Sonia <coleysonia@gmail.com>
2023-04-21 20:37:58 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+// import aos
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+
+// import components
+import Banner from './components/Banner';
+import Header from './components/Header';
+import About from './components/About';
+import Workouts from './components/Workouts';
+import Pricing from './components/Pricing';
+import Community from './components/Community';
+import Faq from './components/Faq';
+import Join from './components/Join';
+import Footer from './components/Footer';
+
+const App = () => {
+  // aos initialization
+  Aos.init({
+    duration: 2500,
+    delay: 400,
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='max-w-[1440px] mx-auto bg-page overflow-hidden relative'>
+      <Header />
+      <Banner />
+      <About />
+      <Workouts />
+      <Pricing />
+      <Community />
+      <Faq />
+      <Join />
+      <Footer />
+      {/* <div className='h-[4000px]'></div> */}
     </div>
   );
-}
+};
 
 export default App;
